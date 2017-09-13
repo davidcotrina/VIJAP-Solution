de411e0f60f5b5ef4cd1fa45412b84a8a05e5708
David Cotrina <david.cotrina@hotmail.com>
2017-09-12 23:55:32 -0500

    Init2

---
+++ ZombieProject/ZombieProject/Game1.cs
@@ -0,0 +1,60 @@
+﻿// Created by: <David Cotrina Jiménez>
+// Date: 12/09/2017 23:24
+// Updated at: 12/09/2017 23:24
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ZombieProject
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            {
+                this.Exit();
+            }
+
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
